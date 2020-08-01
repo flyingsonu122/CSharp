95804ad83b86577fd312adbc4ce452a2f356e2d8
Sonu kumar kushwaha <51280276+flyingsonu122@users.noreply.github.com>
2020-08-01 12:37:21 +0530

    Add project files.

---
+++ Sonu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sonu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
